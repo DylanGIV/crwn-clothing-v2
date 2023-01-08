6ef5a91742e318ae60c15ef48c53f4182922919c
DylanGIV <dylangiv123@gmail.com>
2023-01-08 06:48:33 -0800

    auth changes

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,24 +1,25 @@
 import {
-  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
-import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import './authentication.styles.scss';
 
-const SignIn = () => {
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+
+const Authentication = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
-    <div>
-      <h1>Sign in Page</h1>
-      <button onClick={logGoogleUser}>Sign in with google popup</button>
+    <div className='authentication-container'>
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
